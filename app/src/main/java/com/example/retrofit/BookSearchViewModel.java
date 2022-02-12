1ad972d436574ed7fd473c2d583a0d6bae5e15c0
Marcos Orozco Silva <72904602+MarcosOS98@users.noreply.github.com>
2022-02-11 21:30:37 -0800

    Working Book Search Application

---
+++ app/src/main/java/com/example/retrofit/BookSearchViewModel.java
@@ -0,0 +1,32 @@
+package com.example.retrofit;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.retrofit.VolumesResponse;
+import com.example.retrofit.BookRepository;
+
+public class BookSearchViewModel extends AndroidViewModel {
+    private BookRepository bookRepository;
+    private LiveData<VolumesResponse> volumesResponseLiveData;
+
+    public BookSearchViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void init() {
+        bookRepository = new BookRepository();
+        volumesResponseLiveData = bookRepository.getVolumesResponseLiveData();
+    }
+
+    public void searchVolumes(String keyword, String author) {
+        bookRepository.searchVolumes(keyword, author);
+    }
+
+    public LiveData<VolumesResponse> getVolumesResponseLiveData() {
+        return volumesResponseLiveData;
+    }
+}
